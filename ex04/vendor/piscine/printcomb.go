5e7514991c53343d6b89d434fa6799a04a802e45
tsukasakop <tsukasakop@gmail.com>
2024-06-26 02:15:21 +0900

    ✨ Implement ex04

---
+++ ex04/vendor/piscine/printcomb.go
@@ -0,0 +1,20 @@
+package piscine
+
+import "ft"
+
+func PrintComb() {
+	for l :='0'; l<='7'; l++ {
+		for m := l+1; m <= '8'; m++ {
+			for r := m+1; r <= '9'; r++ {
+				ft.PrintRune(l)
+				ft.PrintRune(m)
+				ft.PrintRune(r)
+				if l != '7' {
+					ft.PrintRune(',')
+					ft.PrintRune(' ')
+				}
+			}
+		}
+	}
+	ft.PrintRune('\n')
+}
